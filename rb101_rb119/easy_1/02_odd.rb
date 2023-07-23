8513c13a3d2416c0d79dc3b74dcf74e2205e96d2
Jack Kalayjian <jackjkal@protonmail.com>
2023-07-23 09:35:18 -0700

    Complete further exploration for exercise 2

--- rb101_rb119/easy_1/02_odd.rb
+++ rb101_rb119/easy_1/02_odd.rb
@@ -11,3 +11,20 @@ puts is_odd?(-17)  # => true
 puts is_odd?(-8)   # => false
 puts is_odd?(0)    # => false
 puts is_odd?(7)    # => true
+
+=begin
+If you weren't certain whether % were the modulo or remainder operator, how
+would you rewrite #is_odd? so it worked regardless?
+
+`number / 2 != number / 2.0` since dividing an odd number by 2 would yield
+a float that would not be equal to the integer.
+
+=====
+
+The `Integer#remainder`` method performs a remainder operation in Ruby. Rewrite
+`#is_odd?`` to use `Integer#remainder` instead of `%`. Note: before version
+2.4, Ruby used the `Numeric#remainder` method instead.
+
+number.remainder(2) != 0
+
+=end
