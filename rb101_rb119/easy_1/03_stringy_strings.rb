cad686557db3d3f490c2961ac4fedc347ab559d8
Jack Kalayjian <jackjkal@protonmail.com>
2023-07-24 17:51:13 -0700

    Complete exercise 3

---
+++ rb101_rb119/easy_1/03_stringy_strings.rb
@@ -0,0 +1,14 @@
+def stringy(number)
+  string = '1'
+
+  while string.length < number
+    string << (string.end_with?('1') ? '0' : '1')
+  end
+
+  string
+end
+
+puts stringy(6) == '101010'
+puts stringy(9) == '101010101'
+puts stringy(4) == '1010'
+puts stringy(7) == '1010101'
