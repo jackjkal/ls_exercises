5c0d06454ac8f46eca2d6a8c5cb52f4cd2ceb573
Jack Kalayjian <jackjkal@protonmail.com>
2023-08-28 22:36:58 -0700

    Complete exercise 7

---
+++ rb101_rb119/easy_1/07_madlibs.rb
@@ -0,0 +1,14 @@
+print "Enter a noun: "
+noun = gets.chomp
+
+print "Enter a verb: "
+verb = gets.chomp
+
+print "Enter an adjective: "
+adjective = gets.chomp
+
+print "Enter an adverb: "
+adverb = gets.chomp
+
+puts "The #{adjective} #{noun} #{verb}s #{adverb} for food."
+puts "When the #{noun} is #{adjective}, it #{verb}s #{adverb}."
