738041fb9f974a807531e661e184bbce6a892482
Jack Kalayjian <jackjkal@protonmail.com>
2023-09-07 22:28:30 -0700

    Complete further exploration for exercise 3

--- rb101_rb119/easy_2/03_tip_calculator.rb
+++ rb101_rb119/easy_2/03_tip_calculator.rb
@@ -5,7 +5,7 @@ print "What is the tip percentage? "
 tip_rate = gets.to_f / 100
 
 tip = (bill * tip_rate).round(2)
-total = (bill + tip).round(2)
+total = format('%.2f', bill + tip)
 
 puts
 puts "The tip is $#{tip}."
